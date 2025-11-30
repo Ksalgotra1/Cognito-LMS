d5e1ff81ad3be4b8972ff30053bba78bf96b346d
Krishna Salgotra <183400124+Ksalgotra1@users.noreply.github.com>
2025-11-30 14:38:18 +0530

    feat: Secure backend with python-dotenv and externalize SECRET_KEY

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -9,18 +9,23 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env_path = BASE_DIR / '.env'
+load_dotenv(dotenv_path=env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mn2ttsrqxl*k$=ot3plq+6x+p5y1d@nl1i^&+52*y%8itci-$1'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+if not SECRET_KEY:
+    raise ValueError("No SECRET_KEY set for Django application")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
